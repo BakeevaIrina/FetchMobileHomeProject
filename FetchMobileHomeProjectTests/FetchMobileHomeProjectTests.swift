5dadf87b07fc0a715963e61f3d277889c39c8b71
BakeevaIrina <114573318+BakeevaIrina@users.noreply.github.com>
2025-03-15 23:15:28 -0700

    Initial Commit

---
+++ FetchMobileHomeProjectTests/FetchMobileHomeProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchMobileHomeProjectTests.swift
+//  FetchMobileHomeProjectTests
+//
+//  Created by Irina Marmyl on 3/15/25.
+//
+
+import Testing
+@testable import FetchMobileHomeProject
+
+struct FetchMobileHomeProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
